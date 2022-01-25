8975e16deff60ee61961aa718c05919d3a398b70
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-25 03:09:48 +0000

    Update require

--- bot.js
+++ bot.js
@@ -1,24 +1,7 @@
 // DiscordJS API initialization
 const { Client, Intents } = require("discord.js");
 var Bot = {};
-Bot.client = new Client({ intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILDS_MEMBERS,
-    Intents.FLAGS.GUILD_BANS,
-    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-    Intents.FLAGS.GUILD_INTEGRATIONS,
-    Intents.FLAGS.GUILD_WEBHOOKS,
-    Intents.FLAGS.GUILD_INVITES,
-    Intents.FLAGS.GUILD_VOICE_STATES,
-    Intents.FLAGS.GUILD_PRESENCES,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    Intents.FLAGS.GUILD_MESSAGE_TYPING,
-    Intents.FLAGS.DIRECT_MESSAGES,
-    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-    Intents.FLAGS.DIRECT_MESSAGE_TYPING,
-    Intents.FLAGS.GUILD_SCHEDULED_EVENTS
-] });
+Bot.client = new Client({ intents: Intents.ALL });
 
 // Command manager initialization
 Bot.commands = {};
