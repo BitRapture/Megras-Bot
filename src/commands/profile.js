218536934c07bf188d1240bc9d8963d33bf2f641
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-25 15:15:36 +0000

    Convert profiles to b&w

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -20,15 +20,19 @@ module.exports = {
                 CTX.drawImage(background, 0, 0, 320, 150);
 
                 loadImage(message.author.displayAvatarURL({ format: "png", size: 128 })).then((profile) => {
+                    CTX.save();
+                    CTX.fillStyle = "#FFF";
+                    CTX.fillRect(10, 10, 110, 110);
+                    CTX.globalCompositeOperation = "luminosity";
                     CTX.drawImage(profile, 10, 10, 110, 110);
+                    CTX.restore();
 
                     loadImage("./src/media/profile overlay.png").then((overlay) => {
                         CTX.drawImage(overlay, 0, 0, 320, 150);
 
 
                         let file = new MessageAttachment(Canvas.toBuffer("image/png"), "profile.png");
-                        let embed = new MessageEmbed({ title: `${message.member.nickname}'s profile`, color: 0x9B59B6 })
-                        .setImage("attachment://profile.png");
+                        let embed = new MessageEmbed({ title: `${message.member.nickname}'s profile`, color: 0x9B59B6 }).setImage("attachment://profile.png");
                         msg.edit({ content: `${message.author}`, embeds: [embed], files: [file] });
                     });
                 });
