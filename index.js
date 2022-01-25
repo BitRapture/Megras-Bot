26dc449011504ec948e4d2ee836b8e8090b2b1a7
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-25 10:28:41 +0000

    Add meta bot controller

---
+++ index.js
@@ -0,0 +1,17 @@
+// Meta bot controller
+const { fork, execSync } = require("child_process");
+
+// Setup bot close listener
+BotProcess.on("close", (code) => {
+    switch (code)
+    {
+        case 2: // Update and restart the bot
+            console.log("Updating the bot through Discord...");
+            execSync("sh ./update.sh");
+            BotProcess = fork("./bot.js");
+        break;
+    }
+})
+
+// Run the bot process
+var BotProcess = fork("./bot.js");
--- update.sh
+++ update.sh
@@ -2,5 +2,4 @@
 
 git pull
 pm2 restart 0
-pm2 logs
 printf "Updated and restarted bot\n"
