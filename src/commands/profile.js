7eb618ebce49cbb73a992cb156318d8912e183fe
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-25 14:15:38 +0000

    Return to promise based method

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -4,9 +4,6 @@ const CTX = Canvas.getContext("2d");
 
 const Embed = require("../templates/embeds.js");
 
-const Background = loadImage("./src/media/profile.png");
-const Overlay = loadImage("./src/media/profile overlay.png");
-
 module.exports = {
     name : "profile",
     desc : "Display your profile",
@@ -18,13 +15,17 @@ module.exports = {
 
     Run(Bot, args, message) {
         message.channel.send({ content: `${message.author}`, embeds: [Embed.SimpleEmbed("Creating profile", "Please wait")] }).then((msg) => {
-            CTX.drawImage(Background, 0, 0);
+            let background = loadImage("./src/media/profile.png").then(() => {
+                CTX.drawImage(background, 0, 0);
+            });
             
             let profile = loadImage(message.author.avatarURL).then(() => {
                 CTX.drawImage(profile, 10, 10, 119, 119);
             });
 
-            CTX.drawImage(Overlay, 0, 0);
+            let overlay = loadImage("./src/media/profile overlay.png").then(() => {
+                CTX.drawImage(overlay, 0, 0);
+            });
             
             let embed = Embed.SimpleEmbed("Profile", `${message.member.nickname}'s profile`);
             embed.image = { url: Canvas.toDataURL(), width: 320, height: 150 };
