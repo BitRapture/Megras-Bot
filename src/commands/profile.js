367a07e089ccc3d9b8f167b13fba8e6de109b8a4
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-25 14:46:09 +0000

    Test inside

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -1,7 +1,5 @@
 const { MessageAttachment } = require("discord.js");
 const { createCanvas, loadImage } = require("canvas");
-const Canvas = createCanvas(320, 150);
-const CTX = Canvas.getContext("2d");
 
 const Embed = require("../templates/embeds.js");
 
@@ -14,21 +12,24 @@ module.exports = {
     ],
     visible : true,
 
+    canvas: createCanvas(320, 150),
+    ctx: this.canvas.getContext("2d"),
+
     Run(Bot, args, message) {
         message.channel.send({ content: `${message.author}`, embeds: [Embed.SimpleEmbed("Creating profile", "Please wait")] }).then((msg) => {
             loadImage("./src/media/profile.png").then((background) => {
-                CTX.drawImage(background, 0, 0, 320, 150);
+                this.ctx.drawImage(background, 0, 0, 320, 150);
             });
             
             loadImage(message.author.displayAvatarURL({ format: "png", size: 128 })).then((profile) => {
-                CTX.drawImage(profile, 10, 10, 119, 119);
+                this.ctx.drawImage(profile, 10, 10, 119, 119);
             });
 
             loadImage("./src/media/profile overlay.png").then((overlay) => {
-                CTX.drawImage(overlay, 0, 0, 320, 150);
+                this.ctx.drawImage(overlay, 0, 0, 320, 150);
             });
             
-            let file = new MessageAttachment(Canvas.toBuffer("image/png"), "profile.png");
+            let file = new MessageAttachment(this.canvas.toBuffer("image/png"), "profile.png");
             let embed = Embed.SimpleEmbed("Profile", `${message.member.nickname}'s profile`);
             embed.image = { url: file.url, width: 320, height: 150 };
             msg.edit({ content: `${message.author}`, embeds: [embed], files: [file] });
