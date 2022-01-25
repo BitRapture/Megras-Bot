9750e51476da0b4befc69856eccfe85d194e6b0b
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-25 08:43:03 +0000

    Add SQL storage

--- bot.js
+++ bot.js
@@ -1,9 +1,19 @@
 // DiscordJS API initialization
 const { Client, Intents } = require("discord.js");
-var Bot = {};
-Bot.client = new Client({ intents: new Intents(0b1111111111111111) });
+var Bot = { client : new Client({ intents: new Intents(0b1111111111111111) }) };
+
+// SQL storage initialization
+const Enmap = require("enmap");
+Bot.store = {
+    dev : new Enmap({name: "dev", fetchAll: false, dataDir: "./store"}),
+    users : {
+        inv : new Enmap({name: "userInv", fetchAll: false, dataDir: "./store"}),
+        lvl : new Enmap({name: "userLvl", fetchAll: false, dataDir: "./store"})
+    }
+};
 
 // Command manager initialization
+const FS = require("fs");
 Bot.commands = {};
 
 // API keys initialization
