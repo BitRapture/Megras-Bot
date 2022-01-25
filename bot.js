30336182ec2fb6ee633590fdd75d2e9cfbeacca7
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-25 09:33:41 +0000

    Update command handling

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 // DiscordJS API initialization
 const { Client, Intents } = require("discord.js");
-var Bot = { client : new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, 
-                                            Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-                                            Intents.FLAGS.DIRECT_MESSAGES
+var Bot = { client : new Client({ intents: [
+    Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, 
+    Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGES
 ] }) };
 
 // SQL storage initialization
@@ -19,7 +20,7 @@ Bot.store = {
 const FS = require("fs");
 const CMDFiles = FS.readdirSync("./src/commands").filter(i => i.endsWith(".js"));
 Bot.commands = new Map();
-CMDFiles.forEach((file) => { let cmd = require(`./src/commands/${file}`); Bot.commands.set(cmd.name, cmd); console.log(file); } );
+CMDFiles.forEach((file) => { const cmd = require(`./src/commands/${file}`); Bot.commands.set(cmd.name, cmd); } );
 
 // API keys initialization
 Bot.keys = require("./src/secret/keys.json");
