517edd90399025581a3f424107f3431ab28caf73
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-26 23:19:29 +0000

    Update replacement

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ CMDFiles.forEach((file) => {
     const cmd = require(`./src/commands/${file}`); 
     Bot.commands.set(cmd.name, cmd); 
     if (cmd.visible) Bot.commandsList.push({ name: cmd.name, desc: cmd.desc }); 
-    cmd.examples.forEach((example) => { example.value.replaceAll("$", Bot.config.prefix); });
+    cmd.examples.forEach((example) => { example.value.replace(/$/g, Bot.config.prefix); });
 });
 
 
