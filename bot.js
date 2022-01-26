97963771dff10d0670004990fa4539948653b33f
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-26 23:27:44 +0000

    Attempt to fix pass by value again

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ CMDFiles.forEach((file) => {
     const cmd = require(`./src/commands/${file}`); 
     Bot.commands.set(cmd.name, cmd); 
     if (cmd.visible) Bot.commandsList.push({ name: cmd.name, desc: cmd.desc }); 
-    cmd.examples.forEach((p, i, example) => { example[i].value.replaceAll(/$/g, Bot.config.prefix); });
+    Bot.commands.get(cmd.name).examples.forEach((p, i, example) => { example[i].value.replace(/$/g, Bot.config.prefix); });
 });
 
 
