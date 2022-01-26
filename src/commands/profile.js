bab44f75357b52557792239f287a4b9e7ebb4740
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-26 17:40:25 +0000

    I am going to strangle js

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -41,7 +41,7 @@ module.exports = {
                         CTX.fillText(bal.toString().substring(0, 6).padStart(7, "0"), 324, 164);
                         
                         // Show nickname
-                        let nickname = member.nickname; nickname = (nickname === null ? member.username : nickname);
+                        let nickname = member.nickname; nickname = (nickname === undefined ? member.username : nickname);
                         CTX.fillText(nickname.substring(0, 8), 254, 36);
 
                         // Upload file and insert into embed
