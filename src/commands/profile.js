81053c6ebc75340e96b4ec29c7ba15e817724cfb
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-26 17:11:45 +0000

    Resize canvas

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -1,8 +1,11 @@
 const { MessageAttachment, MessageEmbed } = require("discord.js");
 const { createCanvas, loadImage, registerFont } = require("canvas");
 registerFont("./src/media/Pixel.ttf", { family: "Pixel" });
-const Canvas = createCanvas(320, 150);
+const Canvas = createCanvas(640, 300);
 const CTX = Canvas.getContext("2d");
+CTX.imageSmoothingEnabled = false;
+CTX.font = "40px Pixel";
+CTX.textBaseline = "top";
 
 const Embed = require("../templates/embeds.js");
 
@@ -18,26 +21,23 @@ module.exports = {
     Run(Bot, args, message) {
         message.channel.send({ content: `${message.author}`, embeds: [Embed.SimpleEmbed("Creating profile", "Please wait")] }).then((msg) => {
             loadImage("./src/media/profile.png").then((background) => {
-                CTX.imageSmoothingEnabled = false;
-                CTX.drawImage(background, 0, 0, 320, 150);
+                CTX.drawImage(background, 0, 0, 640, 300);
 
                 loadImage(message.author.displayAvatarURL({ format: "png", size: 128 })).then((profile) => {
                     // Convert avatar to greyscale
                     CTX.save();
                     CTX.fillStyle = "#FFF";
-                    CTX.fillRect(10, 10, 110, 110);
+                    CTX.fillRect(20, 20, 220, 220);
                     CTX.globalCompositeOperation = "luminosity";
-                    CTX.drawImage(profile, 10, 10, 110, 110);
+                    CTX.drawImage(profile, 20, 20, 220, 220);
                     CTX.restore();
 
                     loadImage("./src/media/profile overlay.png").then((overlay) => {
-                        CTX.drawImage(overlay, 0, 0, 320, 150);
-                        CTX.font = "20px Pixel";
-                        CTX.textBaseline = "top";
+                        CTX.drawImage(overlay, 0, 0, 640, 300);
 
                         // Load player balance
                         let bal = (Bot.store.users.bal.has(message.author.id) ? Bot.store.users.bal.get(message.author.id) : 0);
-                        CTX.fillText(bal.toString().substring(0, 6).padStart(7, "0"), 162, 82);
+                        CTX.fillText(bal.toString().substring(0, 6).padStart(7, "0"), 324, 164);
                         
                         // Upload file and insert into embed
                         let file = new MessageAttachment(Canvas.toBuffer("image/png"), "profile.png");
