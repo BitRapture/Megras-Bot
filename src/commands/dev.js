95c669184303c4e84c78c87b3da5ecf4dda4c4c6
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-31 03:02:47 +0000

    Add quick deletion

--- src/commands/dev.js
+++ src/commands/dev.js
@@ -24,6 +24,11 @@ module.exports = {
                 args.shift();
                 eval(args.join(" "));
             break;
+            case "delLevels":
+                Bot.store.users.exp.forEach((v, k) => {Bot.store.users.exp.delete(k)});
+                Bot.store.users.lvl.forEach((v, k) => {Bot.store.users.lvl.delete(k)});
+                message.reply("Deleted everything");
+            break;
         }
     }
 }
