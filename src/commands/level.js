c256256d07bc90289b73e5a7f9fefaa300c05d2b
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-01-31 03:18:19 +0000

    Add comments

--- src/commands/level.js
+++ src/commands/level.js
@@ -13,10 +13,12 @@ module.exports = {
         let member = message.mentions.members.first(); member = (member === undefined ? message.member : member);
         let nickname = (member.nickname === null ? member.user.username : member.nickname);
 
+        // Get information
         let userExp = Bot.store.users.exp.get(member.id); userExp = (userExp === undefined ? 0 : userExp);
         let userLvl = Bot.store.users.lvl.get(member.id); userLvl = (userLvl === undefined ? 0 : userLvl);
         let nextLevel = Math.floor(((userLvl + 1) ** 1.2) * 500);
 
+        // Convert to progress bar
         let progress = `**${userLvl} ` + "".padStart(Math.ceil((userExp / nextLevel) * 20), "▮").padEnd(20, "▯") + ` ${userLvl + 1}**`;
         let fields = [{ name: "Current EXP", value: `${userExp}`, inline: true }, { name: "EXP Required", value: `${nextLevel}`, inline: true }];
 
