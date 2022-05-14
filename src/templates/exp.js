eedf3d63d7419f64bafae81c17376da7cc0c2e38
BitRapture <62811797+BitRapture@users.noreply.github.com>
2022-05-14 13:56:22 +0100

    The Brappy Patty Secret Forumla(TM)

--- src/templates/exp.js
+++ src/templates/exp.js
@@ -1,6 +1,6 @@
 
 module.exports = {
     GetExperience(_level) {
-        return Math.ceil(((_level) * (_level * 1024) * (_level * 0.65)) + 500);
+        return Math.ceil(((_level) * (_level * 12) * (_level * 0.1435)) + ((_level + 1) * 500));
     }
 }
