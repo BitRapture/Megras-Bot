18d8a71fa370396c693403bd27dac67149f2f5d1
BitRapture <thinzy.business+bitrapture@gmail.com>
2023-01-14 20:31:05 +0000

    Flip common parameter

--- src/templates/embeds.js
+++ src/templates/embeds.js
@@ -1,5 +1,5 @@
 module.exports = {
-    Simple(title, description, color = 0x9B59B6, fields = []) {
+    Simple(title, description, fields = [], color = 0x9B59B6) {
         let embed = { title: title, description: description, color: color };
         if (fields.length > 0) embed.fields = fields;
         return {embeds: [embed]};
