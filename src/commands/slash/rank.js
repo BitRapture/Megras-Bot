8abca617b1626b057e958a1c43daa93a21c4c166
BitRapture <thinzy.business+bitrapture@gmail.com>
2023-01-14 21:39:50 +0000

    Rearrange progress bar

--- src/commands/slash/rank.js
+++ src/commands/slash/rank.js
@@ -20,7 +20,7 @@ module.exports = {
         let deltaNextExp = nextLevelExp - currentLevelExp;
         let deltaExp = Math.ceil((deltaCurrentExp / deltaNextExp) * 10);
 
-        let progressBar = "".padStart(deltaExp, "X").padStart(10, "_");
+        let progressBar = "".padStart(deltaExp - 10, "_").padEnd(deltaExp, "X");
 
         let embed = Bot.Templates.Embeds.Simple(`${memberNickname}'s rank profile`, `${levelInfo.level} **${progressBar}** ${levelInfo.level + 1}`, [
             { name: "Current EXP", value: `${levelInfo.experience}`, inline: true },
